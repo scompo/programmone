465e6d32c3148095b3d5484e564b1daa127a2eb2
Mauro Scomparin <scompo@gmail.com>
2017-11-10 20:26:57 +0100

    Added a test for the main method.

--- src/test/java/tk/scompo/programmone/AppTest.java
+++ src/test/java/tk/scompo/programmone/AppTest.java
@@ -11,6 +11,15 @@ public class AppTest {
     @Test
     public void testAppHasAGreeting() {
             final App classUnderTest = new App();
-            assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+            assertNotNull("App should have a greeting", classUnderTest.getGreeting());
+    }
+
+    @Test
+    public void testMainShouldNotThrowAnException() {
+            try {
+                App.main(new String[]{}));
+            }catch (Exception e) {
+                fail("An exception during main: " + e.getMesage();
+            }
     }
 }
