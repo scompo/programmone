d00accb5063e921b31fe75b95925e381bf953870
Mauro Scomparin <scompo@gmail.com>
2017-11-11 01:37:37 +0100

    Should work as a web app.
    Signed-off-by: Mauro Scomparin <scompo@gmail.com>

---
+++ src/main/java/tk/scompo/programmone/Application.java
@@ -0,0 +1,21 @@
+package tk.scompo.programmone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class for the application
+ */
+@SpringBootApplication
+public class Application {
+
+	/**
+	 * Main entry point.
+	 *
+	 * @param args
+	 *            Command line arguments.
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ src/test/java/tk/scompo/programmone/IndexControllerTest.java
@@ -0,0 +1,28 @@
+package tk.scompo.programmone;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class IndexControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("Programmone index")))
+				.andExpect(content().string(containsString("This is a web application")));
+	}
+}
